70250ef394b3e5dae6ebdeb3eb621eb9cfde533b
Oleksii Morenets <oleksii.morenets@gmail.com>
2026-06-28 18:19:10 +0200

    RestClientConfig

--- auth-api/src/main/java/travel/winwin/authapi/config/RestClientConfig.java
+++ auth-api/src/main/java/travel/winwin/authapi/config/RestClientConfig.java
@@ -1,4 +1,23 @@
 package travel.winwin.authapi.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
 public class RestClientConfig {
+
+    private final AppProperties properties;
+
+    public RestClientConfig(AppProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public RestClient dataApiClient() {
+        return RestClient.builder()
+                .baseUrl(properties.dataApi().url())
+                .defaultHeader("X-Internal-Token", properties.internalToken())
+                .build();
+    }
 }
